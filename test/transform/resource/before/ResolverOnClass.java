4a151d3b46d89687e3e653ade7a388e164bf741e
Tiago Aquino <tiagoaquino@gmail.com>
2018-09-23 17:53:26 -0300

    Resolver javac

---
+++ test/transform/resource/before/ResolverOnClass.java
@@ -0,0 +1,46 @@
+/*
+@lombok.Setter
+class SetterOnClass1 {
+	@lombok.Setter(lombok.AccessLevel.NONE)
+	boolean isNone;
+	boolean isPublic;
+}
+
+@lombok.Setter(lombok.AccessLevel.PROTECTED)
+class SetterOnClass2 {
+	@lombok.Setter(lombok.AccessLevel.NONE)
+	boolean isNone;
+	boolean isProtected;
+	@lombok.Setter(lombok.AccessLevel.PACKAGE)
+	boolean isPackage;
+}
+
+@lombok.Setter(lombok.AccessLevel.PACKAGE)
+class SetterOnClass3 {
+	@lombok.Setter(lombok.AccessLevel.NONE)
+	boolean isNone;
+	boolean isPackage;
+}
+
+@lombok.Setter(lombok.AccessLevel.PRIVATE)
+class SetterOnClass4 {
+	@lombok.Setter(lombok.AccessLevel.NONE)
+	boolean isNone;
+	boolean isPrivate;
+}
+
+@lombok.Setter(lombok.AccessLevel.PUBLIC)
+class SetterOnClass5 {
+	@lombok.Setter(lombok.AccessLevel.NONE)
+	boolean isNone;
+	boolean isPublic;
+}
+*/
+@lombok.Resolver
+class ResolverOnClass6 {
+	
+	private Long id;
+	
+	private String name;
+	
+}
